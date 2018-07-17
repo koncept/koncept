7f72b1cdb32c439381af746b1688d37029d59e80
Showsay You <4kizuki.c10.l65@gmail.com>
2018-07-17 15:25:05 +0900

    Feature: declare DirectiveContainerInterface and DirectiveIteratorInterface

---
+++ modules/request/Components/DirectiveContainerInterface.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace Koncept\Request\Components;
+
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+
+
+/**
+ * [Interface] Container of HTTP or Cookie Directives
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Koncept. All Rights Reserved.
+ * @package koncept/request
+ * @since v1.0.0
+ */
+interface DirectiveContainerInterface
+    extends ArrayAccess, Countable, IteratorAggregate
+{
+    /**
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     *
+     * @return ParametersIteratorInterface
+     *
+     * @internal
+     */
+    public function getIterator(): ParametersIteratorInterface;
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param string $offset an offset to check for.
+     * @return boolean true on success or false on failure.
+     *
+     * @internal
+     */
+    public function offsetExists($offset): bool;
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset the offset to retrieve.
+     * @return string
+     *
+     * @internal
+     */
+    public function offsetGet($offset): string;
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param string $offset the offset to assign the value to.
+     * @param string $value the value to set.
+     * @return void
+     *
+     * @internal
+     */
+    public function offsetSet($offset, $value): void;
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset the offset to unset.
+     * @return void
+     *
+     * @internal
+     */
+    public function offsetUnset($offset): void;
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     *
+     * @return int the custom count as an integer.
+     *
+     * @internal
+     */
+    public function count(): int;
+}
---
+++ modules/request/Components/DirectiveIteratorInterface.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace Koncept\Request\Components;
+
+use Iterator;
+
+
+/**
+ * [Interface] Iterator of Directives
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Koncept. All Rights Reserved.
+ * @package koncept/request
+ * @since v1.0.0
+ */
+interface DirectiveIteratorInterface
+    extends Iterator
+{
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     *
+     * @return string
+     */
+    public function current(): string;
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     *
+     * @return void
+     */
+    public function next(): void;
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     *
+     * @return null|string string on success or null on failure.
+     */
+    public function key(): ?string;
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     *
+     * @return boolean true on success or false on failure.
+     */
+    public function valid(): bool;
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     *
+     * @return void
+     */
+    public function rewind(): void;
+}
